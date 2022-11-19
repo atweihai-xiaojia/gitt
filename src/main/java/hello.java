8f3a011255e1be3903bc59fdd3ae087c18a5dea5
yun <3485133076@qq.com>
2022-11-19 14:15:24 +0800

    hot test

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,12 +1,7 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello2");
-        System.out.println("hello3");
-        System.out.println("hello4");
-        System.out.println("第三次提交");
-        System.out.println("hot -fix 提交");
-        System.out.println("master test");
-        System.out.println("hot test");
+        System.out.println("master 冲突");
+
+
     }
 }
