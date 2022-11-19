49d12a419ead24d18a2c0701350fb14d6ebcca0d
yun <3485133076@qq.com>
2022-11-19 14:16:27 +0800

    master 冲突

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,11 +1,6 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello2");
-        System.out.println("hello3");
-        System.out.println("hello4");
-        System.out.println("第三次提交");
-        System.out.println("hot -fix 提交");
-        System.out.println("hot test");
+        System.out.println("hot 冲突");
     }
 }
